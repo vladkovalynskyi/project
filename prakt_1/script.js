5830ae355687ccdbb4e0b575002e9ef4af08a459
Vlad <example@gmail.com>
2022-02-05 13:55:40 +0200

    my second and finish try

--- prakt_1/script.js
+++ prakt_1/script.js
@@ -1,2 +1,19 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = +prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = +prompt('На сколько оцените его?','');
+
+personalMovieDB.movie[a] = b;
+personalMovieDB.movie[c] = d;
+
+console.log(personalMovieDB);
