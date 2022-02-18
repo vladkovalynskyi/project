f4762fb42316bd3d08aa398d23d47706e46d9415
Vlad <example@gmail.com>
2022-02-18 16:43:51 +0200

    my pract_2  and 3

---
+++ prakt_2/script.js
@@ -0,0 +1,39 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+for (let i=0; i < 2; i++) {
+    
+    const a = prompt('Один из просмотренных фильмов?',''),
+          b = prompt('На сколько оцените его?','');   
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movie[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+if (personalMovieDB.count<10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count>10 && personalMovieDB.count<30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count>30) {
+    console.log('Вы киноман');
+} else {
+    console.log('error');
+}
+    
+
+
+console.log(personalMovieDB);
+
+const logg = "Sword Art Online";
+
+console.log(logg.slice(4, 11));
